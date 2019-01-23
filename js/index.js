27684643d0f9d6e8a0d659b84e67cb6a4b4573bc
pulin84 <31483490+pulin84@users.noreply.github.com>
2019-01-23 15:17:43 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+   $(document).ready(function() {
+	  
+	      $('.nav-link').css('visibility','visible').hide().fadeIn("3000");
+
+	  /* $(document).on('scroll', function (){
+		   var curr = $(this).scrollTop();
+		   
+		   $('.container,.container-fluid').each(function(){
+			   if(curr > $(this).offset().top) $(this).css('opacity', 1 - (curr- $(this).offset().top)/400);
+		   });
+	   }); */
+	   
+   });
